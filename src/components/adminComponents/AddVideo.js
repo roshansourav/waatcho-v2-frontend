5eb9a3388f218538ff554fd45aed9670a7d68900
roshansourav <roshansouravj@gmail.com>
2021-08-03 01:22:49 +0530

    progress upload spinner added

--- src/components/adminComponents/AddVideo.js
+++ src/components/adminComponents/AddVideo.js
@@ -1,37 +1,50 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { uploadFile } from 'react-s3';
 import axios from 'axios';
 import "./addvideo.css";
+import progress_img from "../../assets/progressIMG.gif";
 
+const S3_BUCKET = '';
+const REGION = '';
+const ACCESS_KEY = '';
+const SECRET_ACCESS_KEY = '';
 
-const S3_BUCKET = 'BUCKET_NAME';
-const REGION = 'REGION';
-const ACCESS_KEY = 'KEY_ID';
-const SECRET_ACCESS_KEY = 'SECRET_ACCESS';
+class AddVideo extends React.Component {
 
-const config = {
-    bucketName: S3_BUCKET,
-    region: REGION,
-    accessKeyId: ACCESS_KEY,
-    secretAccessKey: SECRET_ACCESS_KEY,
-}
+    constructor(props) {
+        super(props)
+        this.state = {
+            selectedFile: null,
+            progressState : 'none'
+        }
 
-const AddVideo = () => {
-
-    const [selectedFile, setSelectedFile] = useState(null);
-
-    const handleFileInput = (e) => {
-        setSelectedFile(e.target.files[0]);
-    }
-    function fileSizeConverter(sizeInBytes){
-        return (sizeInBytes/1048576).toFixed(2) + " MB";
+        this.fileData = this.fileData.bind(this);
+        this.handleUpload = this.handleUpload.bind(this);
+        this.handleFileInput = this.handleFileInput.bind(this);
+        this.fileSizeConverter = this.fileSizeConverter.bind(this);
     }
 
-    const handleUpload = async (file) => {
-        uploadFile(file, config)
+    config = {
+        bucketName: S3_BUCKET,
+        region: REGION,
+        accessKeyId: ACCESS_KEY,
+        secretAccessKey: SECRET_ACCESS_KEY,
+    }
+
+    handleFileInput = (e) => {
+        // setSelectedFile(e.target.files[0]);
+        this.setState({ selectedFile: e.target.files[0] })
+    }
+    fileSizeConverter(sizeInBytes) {
+        return (sizeInBytes / 1048576).toFixed(2) + " MB";
+    }
+
+    handleUpload = async (file) => {
+        this.setState({progressState : 'block'});
+        uploadFile(file, this.config)
             .then(data => {
+                this.setState({progressState : 'none'});
                 console.log(data);
-                // window.alert(fileSizeConverter(file.size));
 
                 //axios started here
                 const fileDetails = {
@@ -39,10 +52,10 @@ const AddVideo = () => {
                     "file_name": data.key,
                     "file_type": file.type,
                     "file_date_time": new Date().toISOString(),
-                    "file_size": fileSizeConverter(file.size),
-                    "uploaded_by_id": "admin_hard_coded"
+                    "file_size": this.fileSizeConverter(file.size),
+                    "uploaded_by_id": "admin_hard_coded" //later I will add admin_id here
                 }
-                
+
                 axios.post("http://localhost:8080/addVideo", fileDetails).then(() => {
                     window.alert("Uploaded Successfully");
                     window.location.reload();
@@ -52,51 +65,55 @@ const AddVideo = () => {
             .catch(err => console.error(err))
     }
 
-    function fileData(selectedFile){
-            if (selectedFile) {
-    
-                return (
-                    <div>
-                        <h5>File Details:</h5>
-                        <p className="card-text">File Name: {selectedFile.name}</p>
-                        <p className="card-text">File Type: {selectedFile.type}</p>
-                    </div>
-                );
-            } else {
-                return (
-                    <div>
-                        <br />
-                        <h4>Choose before Pressing the Upload button</h4>
-                    </div>
-                );
-            } 
-    }
+    fileData(selectedFile) {
+        if (selectedFile) {
 
-
-    return (
-
-        <div>
-            <div id="stars"></div>
-            <div class="card text-center border border-warning">
-                <div class="card-header bg-warning text-dark">
-                    Upload
+            return (
+                <div>
+                    <h5>File Details:</h5>
+                    <p className="card-text">File Name: {selectedFile.name}</p>
+                    <p className="card-text">File Type: {selectedFile.type}</p>
                 </div>
-                <div class="card-body">
-                    <h5 class="card-title">Upload New Video File Here</h5>
-                    <input className="form-control" type="file" onChange={handleFileInput} />
+            );
+        } else {
+            return (
+                <div>
                     <br />
-                    <button class="btn btn-success btn-width" onClick={() => handleUpload(selectedFile)}>
-                        Upload!
-                    </button>
+                    <h4>Choose before Pressing the Upload button</h4>
                 </div>
-                <div class="card-footer bg-warning text-dark">
-                    {fileData(selectedFile)}
+            );
+        }
+    }
+    
+
+
+    render() {
+
+        return (
+            <div>
+                <div id="stars"></div>
+                <div class="card text-center border border-warning">
+                    <div class="card-header bg-warning text-dark">
+                        Upload
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Upload New Video File Here</h5>
+                        <input className="form-control" type="file" onChange={this.handleFileInput} />
+                        <br />
+                        <div className="myProgress" style={{display : this.state.progressState}}>
+                            <img src={progress_img} alt="progress-img" width="150" height="150" />
+                        </div>
+                        <button class="btn btn-success btn-width" onClick={() => this.handleUpload(this.state.selectedFile)}>
+                            Upload!
+                        </button>
+                    </div>
+                    <div class="card-footer bg-warning text-dark">
+                        {this.fileData(this.state.selectedFile)}
+                    </div>
                 </div>
             </div>
-        </div>
-
-
-    );
+        )
+    }
 }
 
 export default AddVideo;
